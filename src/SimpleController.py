d399cf596df8c13fdc0dfb89a3195b1a8973422f
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-12-20 16:02:55 +0800

    Update rule in SimpleController.py

--- src/SimpleController.py
+++ src/SimpleController.py
@@ -96,7 +96,7 @@ class SimpleController1(app_manager.RyuApp):
                 ipv4_dst="10.0.0.2",
                 ip_proto=17,
                 udp_dst=5566)
-            actions = [parser.OFPActionOutput(2)]
+            actions = [parser.OFPActionOutput(3)]
             self.add_flow(
                 datapath=datapath,
                 priority=3,
